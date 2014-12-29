78116fb4f9ee316a17bb0366e0f64606ed973c3e
薛祥清 <990828785@qq.com>
2014-12-29 23:23:13 +0800

    12.29:23

--- src/com/mlegeb/core/Massager.java
+++ src/com/mlegeb/core/Massager.java
@@ -1,14 +1,24 @@
 package com.mlegeb.core;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
 /**
  * 数据处理者
  * @author Mine
  */
 public class Massager {
 	 private static Massager massager;
-
+	 private static Robot robot;
+	 
      private Massager() {
-         
+         try {
+			robot = new Robot();
+		} catch (AWTException e) {
+			System.out.println("无法获取键盘");
+			e.printStackTrace();
+		}
      }
      
      /**
@@ -19,5 +29,37 @@ public class Massager {
          if (massager == null)
              return new Massager();
          return massager;
+     }
+     
+     /**
+      * 按下
+      * @param key 按键值
+      */
+     public void keyDown(int key){
+    	 robot.keyPress(key);
+     }
+     
+     public void keyUp(int key){
+    	 robot.keyRelease(key);
+     }
+     
+     public void keyDownAndUp(int key){
+    	 robot.keyPress(key);
+    	 robot.keyRelease(key);
+     }
+     
+     public void keyWithAlt(int key){
+    	 robot.keyPress(KeyEvent.VK_ALT);
+    	 robot.keyPress(key);
+    	 robot.keyRelease(key);
+    	 robot.keyRelease(KeyEvent.VK_ALT);
+     }
+     
+     public void mouseMove(float x, float y){
+    	 
+     }
+     
+     public void clickMove(){
+    	 
      }
 }
--- src/com/mlegeb/state/PPTState.java
+++ src/com/mlegeb/state/PPTState.java
@@ -1,4 +1,8 @@
 package com.mlegeb.state;
+
+import java.awt.event.KeyEvent;
+
+
 /**
  * PPT状态
  * @author Mine
@@ -6,10 +10,42 @@ package com.mlegeb.state;
  */
 public class PPTState extends ControlState{
 
+	private static final String PAGE_UP   = "0";
+	private static final String PAGE_DOWN = "1";
+	private static final String PLAY      = "2";
+	private static final String EXIT      = "3";
+	private static final String CLOSE     = "4";
+	private static final String MODEL_N   = "5";
+	private static final String MODEL_S   = "6";
+	
 	@Override
 	public void AcceptData(String data) {
-		// TODO Auto-generated method stub
-		
+		switch (data) {
+		case PAGE_UP:
+			massager.keyDownAndUp(KeyEvent.VK_PAGE_UP);
+			break;
+		case PAGE_DOWN:
+			massager.keyDownAndUp(KeyEvent.VK_PAGE_DOWN);
+			break;
+		case PLAY:
+			massager.keyDownAndUp(KeyEvent.VK_F5);
+			break;
+		case EXIT:
+			massager.keyDownAndUp(KeyEvent.VK_ESCAPE);
+			break;
+		case CLOSE:
+			massager.keyWithAlt(KeyEvent.VK_F4);
+			break;
+		case MODEL_N:
+			massager.keyWithAlt(KeyEvent.VK_V);
+			massager.keyDownAndUp(KeyEvent.VK_N);
+			break;
+		case MODEL_S:
+			massager.keyWithAlt(KeyEvent.VK_V);
+			massager.keyDownAndUp(KeyEvent.VK_S);
+			break;
+
+		}
 	}
 
 }
