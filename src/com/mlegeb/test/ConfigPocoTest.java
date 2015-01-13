e9b952a1250bc9c65055eca2c86f774f9f2b528c
薛祥清 <990828785@qq.com>
2015-01-14 01:03:35 +0800

    01.14:01

--- src/com/mlegeb/test/ConfigPocoTest.java
+++ src/com/mlegeb/test/ConfigPocoTest.java
@@ -1,7 +1,10 @@
 package com.mlegeb.test;
 
+import java.awt.event.KeyEvent;
+
 import org.junit.Test;
 
+import com.mlegeb.config.GameConfigPoco;
 import com.mlegeb.core.Massager;
 
 public class ConfigPocoTest {
@@ -14,6 +17,20 @@ public class ConfigPocoTest {
 	
 	@Test
 	public void getTest(){
+		
+		GameConfigPoco config = new GameConfigPoco();
+		config.i_A = KeyEvent.VK_I;
+		config.i_B = KeyEvent.VK_J;
+		config.i_C = KeyEvent.VK_K;
+		config.i_D = KeyEvent.VK_L;
+		
+		config.i_down = KeyEvent.VK_S;
+		config.i_up = KeyEvent.VK_W;
+		config.i_left = KeyEvent.VK_A;
+		config.i_right = KeyEvent.VK_D;
+		config.saveConfig();
+		
+		
 	}
 	
 
